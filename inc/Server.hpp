4139e73f1fa1e9757bce29cf69e21b3464563c39
bbento-a <bbento-a@student.42lisboa.com>
2026-03-17 10:35:56 +0000

    Added a server class concept in header

---
+++ inc/Server.hpp
@@ -0,0 +1,33 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+// Notes from metro thinking time aahh (it means this needs reviewing)
+/* 
+class Server
+{
+	private:
+	Clients[] //connected clients to the sv
+	Channels[] //existing channels at sv
+	
+	public:
+	//OCF
+	ServerStart() // check everything for start up
+	ServerRuntime() //loop of connections
+	RegisterClient() // check client infos to link them to the sv
+	ConnectClient() //maybe?
+};
+
+class Channel
+{
+	private:
+	Clients[] // these are the ones connected to the channel
+	// should the ops be a derived from client?
+	//sm more important cmds (like ban, kick)
+
+	public:
+	//OCF
+	//most cmds (like join, msg)
+};
+*/
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "../inc/Server.hpp"
+
+// ./ircserv <port> <password>
+
+int main (int argc, char **argv)
+{
+
+}
